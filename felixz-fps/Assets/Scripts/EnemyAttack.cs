dfac3f9d176565fc49eea10e641b6575a29b9379
zfelix <09felix26@gmail.com>
2022-05-02 23:52:31 -0700

    Fixed Level Issues

--- felixz-fps/Assets/Scripts/EnemyAttack.cs
+++ felixz-fps/Assets/Scripts/EnemyAttack.cs
@@ -6,7 +6,6 @@ public class EnemyAttack : MonoBehaviour
 {
     PlayerHealth target;
     [SerializeField] float damageAmount = 25f;
-    [SerializeField]  
 
     // Start is called before the first frame update
     void Start()
